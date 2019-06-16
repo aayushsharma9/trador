23f6159258ba4e319ca41d208551fb4fcf8390cc
aayushsharma9 <aayush.sharma317@gmail.com>
2019-06-16 23:03:53 +0530

    Initial Commit. Added Google OAuth and Stripe billing

---
+++ client/src/components/Header.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Payment from './Payment';
+
+class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null: return;
+            case false:
+                return (
+                    <li><a href='/auth/google'> Sign in with Google </a></li>
+                );
+            default:
+                return [
+                    <li key='1'><Payment /></li>,
+                    <li key='2'><span style={{ margin: '0 10px' }}>Credits: {this.props.auth.credits}</span></li>,
+                    <li key='3'><a href='/api/logout'> Logout </a></li>
+                ];
+        }
+    }
+
+    render() {
+        return (
+            <nav>
+                <div className='nav-wrapper'>
+                    <Link
+                        to={this.props.auth ? '/surveys' : '/'}
+                        className='left brand-logo'
+                    >
+                        Trador
+                    </Link>
+                    <ul className='right'>
+                        {this.renderContent()}
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+function mapStateToProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h2 style={{ fontWeight: 'bolder' }}>Welcome</h2>
+            <h6>Collect feedback for your application from your users</h6>
+        </div>
+    );
+};
+
+export default Landing;
