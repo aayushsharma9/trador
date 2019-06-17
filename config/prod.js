f5119d0b35587974db2bdd0a08e3870e5514d6dd
aayushsharma9 <aayush.sharma317@gmail.com>
2019-06-17 06:02:06 +0530

    Fixed typo in prod config

--- config/prod.js
+++ config/prod.js
@@ -2,7 +2,7 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     facebookAppID: process.env.FACEBOOK_APP_ID,
-    facebokAppSecret: process.env.FACEBOOK_APP_SECRET,
+    facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
     mongoURI: process.env.MONGO_URI,
     cookieKey: process.env.COOKIE_KEY,
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
