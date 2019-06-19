271de1f5a1309d4dde8d31a0dd0e8815fd3e2258
aayushsharma9 <aayush.sharma317@gmail.com>
2019-06-19 11:28:52 +0530

    Completed product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('products');
 
 module.exports = app => {
-    app.post('/api/products', requireLogin, (req, res) => {
-        console.log('LOG TAG: ', req.body);
+    app.post('/api/products/new', requireLogin, (req, res) => {
         const { name, price, condition, description, category, subCategory } = req.body;
 
         const product = new Product({
@@ -21,9 +20,40 @@ module.exports = app => {
 
         try {
             product.save();
-            res.send(product);
+            res.send({ success: true });
         } catch (err) {
             res.status(422).send(err);
+            res.send({ success: false });
         }
+    });
+
+    app.get('/api/products/all', (req, res) => {
+        Product.find({}, (err, products) => {
+            if (err) {
+                res.send({ success: false });
+            } else {
+                res.send(products);
+            }
+        });
+    });
+
+    app.delete('/api/products/delete', requireLogin, (req, res) => {
+        const { id } = req.body;
+        Product.findOneAndDelete({ _id: id }, (err, doc) => {
+            if (err) {
+                res.send({ success: false });
+            } else {
+                res.send({ success: true });
+            }
+        });
+    });
+
+    app.put('/api/products/update', requireLogin, (req, res) => {
+        if (req.body._user !== req.user.id) return;
+        const { id, name, price, condition, description, category, subCategory } = req.body;
+        Product.updateOne({ _id: id }, req.body, (err, raw) => {
+            if (err) res.send({ success: false });
+            else res.send({ success: true });
+        });
     });
 };
